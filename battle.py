8f4e9c2ffab93c22f9de0de1109852a6fcd00259
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2024-02-12 11:09:51 -0600

    Added Conditionals
    asertgsdrfgarewr

--- battle.py
+++ battle.py
@@ -15,20 +15,25 @@ def attack():
     print(f"1. Light Attack | AP: {player.atk1ap} | DMG: {player.atk1}")
     print(f"2. Heavy Attack | AP: {player.atk2ap} | DMG: {player.atk2}")
     action = int(input(">> "))
-    if action == 1:
+    if action == 1 and player.ap > 0:
         enemy.hp -= player.atk1
         player.ap -= player.atk1ap
         print(f"You did {player.atk1} damage")
-    elif action == 2:
+    elif action == 2 and player.ap > 2:
         oooooooooooooooooo = randint(player.atk2/2, player.atk2)
         enemy.hp -= oooooooooooooooooo
         player.ap -= player.atk2ap
         print(f"You did {oooooooooooooooooo} damage")
+    else:
+        print("You're too tired to act")
 def defend():
-    player.hp -= 4
-    player.ap -= 1
+    if player.ap > 0:
+        player.hp -= 4
+        player.ap -= 1
+    else:
+        print("You're too tired to act")
 def wait():
-    player.ap += 1
+    player.ap += 2
 def enemy_turn():
     if enemy.ap >= 3:
         oooooooooooooooooo = randint(enemy.atk2/2, enemy.atk2)
@@ -36,7 +41,7 @@ def enemy_turn():
         enemy.ap -= enemy.atk2ap
         print(f"You took {oooooooooooooooooo} damage")
     elif enemy.ap == 0:
-        enemy.ap += 1
+        enemy.ap += 2
         print("The enemy recovers")
     elif enemy.hp <= 5:
         enemy.hp += 3
